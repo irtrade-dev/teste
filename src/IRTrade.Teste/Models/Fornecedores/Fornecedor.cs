82f488447900e699a81678aeb10015608cd7d27d
fernandodinardo <dinardo.lazarin@gmail.com>
2020-06-25 23:14:13 -0300

    Criada a Class Models Telefone.

--- src/IRTrade.Teste/Models/Fornecedores/Fornecedor.cs
+++ src/IRTrade.Teste/Models/Fornecedores/Fornecedor.cs
@@ -1,19 +1,18 @@
-﻿using System;
+﻿using IRTrade.Teste.Models.Telefones;
+using System;
+using System.Collections.Generic;
 
 namespace IRTrade.Teste.Models.Fornecedores
 {
     public class Fornecedor
     {
-        //public string Empresa { get; set; } (Eu tenho que puxar a outra Classe Empresa
-
         //Aqui são as propriedades que o Cadastro de Fornecedor deve receber.
         public string Nome { get; set; }
         public string CpfCnpj { get; set; }
         public string Rg { get; set; }
         public DateTime DataNascimento { get; set; }
         public DateTime DataCadastro { get; set; }
-        public string TelefoneFixo { get; set; }
-        public string TelefoneCelular { get; set; }
+        public List<Telefone> Telefones { get; set; }
 
         //Aqui são os dados que cada campo do cadastro de fornecedor irá receber.
         public Fornecedor(string nome,
@@ -21,16 +20,14 @@ namespace IRTrade.Teste.Models.Fornecedores
                           string rg,
                           DateTime dataNascimento,
                           DateTime dataCadastro,
-                          string telefoneFixo,
-                          string telefoneCelular)
+                          List<Telefone> telefones)
         {
             Nome = nome;
             CpfCnpj = cpfCnpj;
             Rg = rg;
             DataNascimento = dataNascimento;
             DataCadastro = dataCadastro;
-            TelefoneFixo = telefoneFixo;
-            TelefoneCelular = telefoneCelular;
+            Telefones = telefones;
         }
 
         public Fornecedor() { }
---
+++ src/IRTrade.Teste/Models/Telefones/Telefone.cs
@@ -0,0 +1,18 @@
+﻿namespace IRTrade.Teste.Models.Telefones
+{
+    public class Telefone
+    {
+        public string CodigoPais { get; set; }
+        public string Ddd { get; set; }
+        public string Numero { get; set; }
+
+        public Telefone(string codigoPais, string ddd, string numero)
+        {
+            CodigoPais = codigoPais;
+            Ddd = ddd;
+            Numero = numero;
+        }
+
+        public Telefone() { }
+    }
+}
