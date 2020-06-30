19332f15f3a851774569f10d6c1c96dc078e67ed
fernandodinardo <dinardo.lazarin@gmail.com>
2020-06-29 22:53:49 -0300

    Criada a classe de contexto.

---
+++ src/IRTrade.Teste/Data/TestDbContext.cs
@@ -0,0 +1,17 @@
+﻿using IRTrade.Teste.Models.Empresas;
+using IRTrade.Teste.Models.Fornecedores;
+using Microsoft.EntityFrameworkCore;
+
+namespace IRTrade.Teste.Data
+{
+    public class TestDbContext : DbContext
+    {
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
