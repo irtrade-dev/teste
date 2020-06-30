706f146825f67d970731b62476a7a7ea7e28ccf4
fernandodinardo <dinardo.lazarin@gmail.com>
2020-06-29 22:55:52 -0300

    Criadas as classes de repositório parcial.

---
+++ src/IRTrade.Teste/Services/Fornecedores/FornecedorRepository.cs
@@ -0,0 +1,25 @@
+﻿using IRTrade.Teste.Interfaces.Fornecedores;
+using IRTrade.Teste.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
+
+namespace IRTrade.Teste.Services.Fornecedores
+{
+    public class FornecedorRepository : GenericRepository<Fornecedor>, IFornecedorRepository
+    {
+        public Fornecedor ObterPorCpfCnpj(string cpfCnpj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Fornecedor> ObterPorDataCadastro(DateTime dataCadastro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Fornecedor> ObterPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/IRTrade.Teste/Services/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using IRTrade.Teste.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace IRTrade.Teste.Services
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public void Atualizar(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ObterPorId(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Salvar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
